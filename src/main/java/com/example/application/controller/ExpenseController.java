59f524b6a65ca8541c2f449efb483a7ddd57b097
Alex Xenon <b3haply@gmail.com>
2023-06-07 23:17:18 +0300

    Finished controller

--- src/main/java/com/example/application/controller/ExpenseController.java
+++ src/main/java/com/example/application/controller/ExpenseController.java
@@ -1,34 +1,33 @@
-//package com.example.application.controller;
-//
-//import com.example.application.model.Expense;
-//import com.example.application.service.ExpenseService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api/expense")
-//public class ExpenseController {
-//
-//    @Autowired
-//    ExpenseService expenseService;
-//
-//    @GetMapping("/all")
-//    public List<Expense> getAllExpenses() {
-//        return expenseService.getAllExpenses();
-//    }
-//
-//    @PostMapping("/add")
-//    public Expense addExpense(@RequestBody Expense expense) {
-//        return expenseService.saveExpense(expense);
-//    }
-//
-//    @PostMapping("/delete")
-//    public String deleteExpense(@RequestBody Expense expense) {
-//        expenseService.deleteExpense(expense);
-//        return "The expense was deleted succesfully";
-//    }
-//
-//}
+package com.example.application.controller;
+
+import com.example.application.model.Expense;
+import com.example.application.service.ExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/expense")
+public class ExpenseController {
+
+    @Autowired
+    ExpenseService expenseService;
+
+    @GetMapping("/all")
+    public List<Expense> getAllExpenses() {
+        return expenseService.getAllExpenses();
+    }
+
+    @PostMapping("/add")
+    public Expense addExpense(@RequestBody Expense expense) {
+        return expenseService.saveExpense(expense);
+    }
+
+    @PostMapping("/delete")
+    public String deleteExpense(@RequestBody Expense expense) {
+        expenseService.deleteExpense(expense);
+        return "The expense was deleted succesfully";
+    }
+
+}
