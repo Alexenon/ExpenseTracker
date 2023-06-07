9c24a7a38bafe962d8c00373ea817f638a033143
Alex Xenon <b3haply@gmail.com>
2023-06-07 22:58:18 +0300

    Fixed bug - no grid displayed

--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -1,24 +1,28 @@
 package com.example.application.views.main;
 
 import com.example.application.model.Expense;
+import com.example.application.model.Timestamp;
 import com.example.application.service.ExpenseService;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Date;
 import java.util.List;
 
 @PageTitle("Main")
 @Route(value = "")
-public class MainView extends HorizontalLayout {
+public class MainView extends VerticalLayout {
 
     private final ExpenseService expenseService;
 
@@ -55,26 +59,19 @@ public class MainView extends HorizontalLayout {
 
     private Component getGrid() {
         grid.addClassName("expenses-grid");
-        grid.setSizeFull();
         grid.setColumns("name", "amount", "description", "date", "timestamp");
         grid.getColumns().forEach(c -> c.setAutoWidth(true));
 
         grid.addColumn(person -> createButton("Add", VaadinIcon.PLUS))
                 .setHeader("Add")
-                .setFlexGrow(0)
-                .setWidth("100px")
                 .setResizable(false);
 
         grid.addColumn(person -> createButton("Edit", VaadinIcon.EDIT))
                 .setHeader("Edit")
-                .setFlexGrow(0)
-                .setWidth("100px")
                 .setResizable(false);
 
         grid.addColumn(person -> createButton("Remove", VaadinIcon.TRASH))
                 .setHeader("Remove")
-                .setFlexGrow(0)
-                .setWidth("100px")
                 .setResizable(false);
 
         updateGrid();
