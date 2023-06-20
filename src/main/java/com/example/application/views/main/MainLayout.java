2bfb9cb4a7b2f38a9db5c8e8a2fe921eb367878c
Alex Xenon <b3haply@gmail.com>
2023-06-20 22:40:23 +0300

    Upgraded header

--- src/main/java/com/example/application/views/main/MainLayout.java
+++ src/main/java/com/example/application/views/main/MainLayout.java
@@ -5,6 +5,9 @@ import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.IconFactory;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.RouterLink;
@@ -26,21 +29,27 @@ public class MainLayout extends AppLayout {
 
         Div navbar = new Div();
         navbar.setId("navbar");
-        final RouterLink home = new RouterLink("Home", MainView.class);
-        final RouterLink dashboard = new RouterLink("Dashboard", DashboardView.class);
 
         Div innerMenu = new Div();
         innerMenu.setClassName("menu-inner");
 
         Span text = new Span("LOGO");
         text.addClassName("logo");
-        navbar.add(text, innerMenu);
+
+        Icon personIcon = new Icon(VaadinIcon.USER);
+        personIcon.addClassName("margin-end");
+
+        final RouterLink home = new RouterLink("Home", MainView.class);
+        final RouterLink dashboard = new RouterLink("Dashboard", DashboardView.class);
+        final RouterLink statistics = new RouterLink("Statistics", DashboardView.class);
+        final RouterLink contact = new RouterLink("Contact", DashboardView.class);
 
         List<Component> listOfRoutes = List.of(home, dashboard);
         listOfRoutes.forEach(e -> e.setClassName("menu-item"));
         innerMenu.add(listOfRoutes);
 
-        header.add(navbar);
+        navbar.add(text, innerMenu);
+        header.add(navbar, personIcon);
         addToNavbar(header);
     }
 
