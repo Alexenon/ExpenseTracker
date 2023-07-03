eac567c73e828bcdcb56bebfe8169f735515cdaf
Alex Xenon <b3haply@gmail.com>
2023-07-03 19:52:00 +0300

    Refactor SecurityService -> getAuthenticatedUser()

--- src/main/java/com/example/application/security/SecurityService.java
+++ src/main/java/com/example/application/security/SecurityService.java
@@ -1,27 +1,15 @@
 package com.example.application.security;
 
 import com.vaadin.flow.spring.security.AuthenticationContext;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
 public record SecurityService(AuthenticationContext authenticationContext) {
 
-    //    // TODO: Add AuthenticationException
-//    public UserDetails getAuthenticatedUser() {
-//        return authenticationContext.getAuthenticatedUser(UserDetails.class).orElseThrow();
-//    }
-
+    // TODO: Add AuthenticationException
     public UserDetails getAuthenticatedUser() {
-        SecurityContext context = SecurityContextHolder.getContext();
-        Object principal = context.getAuthentication().getPrincipal();
-        if (principal instanceof UserDetails) {
-            return (UserDetails) context.getAuthentication().getPrincipal();
-        }
-
-        return null;
+        return authenticationContext.getAuthenticatedUser(UserDetails.class).orElseThrow();
     }
 
     public void logout() {
