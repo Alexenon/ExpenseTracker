1628193b6905823db1b3ace9063419986bc7b8ab
Alex Xenon <b3haply@gmail.com>
2023-07-12 01:08:30 +0300

    Fixed the bug

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,6 +1,7 @@
 package com.example.application;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.AppShellSettings;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,6 +22,12 @@ public class Application extends SpringBootServletInitializer implements AppShel
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(Application.class);
+    }
+
+    @Override
+    public void configurePage(AppShellSettings settings) {
+        settings.addLink("shortcut icon", "icons/icon.png");
+        settings.addFavIcon("icon", "icons/icon.png", "48x48");
     }
 
 }
