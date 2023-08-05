3f62b54eb2934b2b77ded4c5b449cef0d22d74bf
Alex Xenon <b3haply@gmail.com>
2023-08-05 16:30:26 +0300

    Added templates for expenses yearly chart

--- frontend/themes/light_theme/components/javascript/expensesAllTime.js
+++ frontend/themes/light_theme/components/javascript/expensesAllTime.js
@@ -3,33 +3,44 @@
 // https://echarts.apache.org/examples/en/editor.html?c=dataset-link
 // https://echarts.apache.org/examples/en/editor.html?c=calendar-vertical
 
+// DEMO CLASS
+
 option = {
-  legend: {},
-  tooltip: {},
   dataset: {
     source: [
-      ['product', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2023'],
-      ['Matcha Latte', 41.1, 30.4, 65.1, 53.3, 86.5, 92.1, 85.7, 83.1],
-      ['Milk Tea', 86.5, 92.1, 85.7, 83.1, 86.5, 92.1, 85.7, 83.1],
-      ['Cheese Cocoa', 24.1, 67.2, 79.5, 86.4, 24.1, 67.2, 79.5, 86.4],
-      ['True', 24.1, 67.2, 79.5, 86.4]
+      ['amount', 'product'],
+      [58212, 'Matcha Latte'],
+      [78254, 'Milk Tea'],
+      [41032, 'Cheese Cocoa'],
+      [12755, 'Cheese Brownie'],
+      [20145, 'Matcha Cocoa'],
+      [79146, 'Tea'],
+      [91852, 'Orange Juice'],
+      [101852, 'Lemon Juice'],
+      [20112, 'Walnut Brownie']
     ]
   },
-  xAxis: [
-    { type: 'category', gridIndex: 0 },
-    { type: 'category', gridIndex: 1 }
-  ],
-  yAxis: [{ gridIndex: 0 }, { gridIndex: 1 }],
-  grid: [{ bottom: '55%' }, { top: '55%' }],
+  grid: { containLabel: true },
+  xAxis: { name: 'amount' },
+  yAxis: { type: 'category' },
+  visualMap: {
+    orient: 'horizontal',
+    left: 'center',
+    min: 0,
+    max: 100000,
+    text: ['High Score', 'Low Score'],
+    dimension: 0,
+    inRange: {
+      color: ['#65B581', '#FFCE34', '#FD665F']
+    }
+  },
   series: [
-    // These series are in the first grid.
-    { type: 'bar', seriesLayoutBy: 'row' },
-    { type: 'bar', seriesLayoutBy: 'row' },
-    { type: 'bar', seriesLayoutBy: 'row' },
-    // These series are in the second grid.
-    { type: 'bar', xAxisIndex: 1, yAxisIndex: 1 },
-    { type: 'bar', xAxisIndex: 1, yAxisIndex: 1 },
-    { type: 'bar', xAxisIndex: 1, yAxisIndex: 1 },
-    { type: 'bar', xAxisIndex: 1, yAxisIndex: 1 }
+    {
+      type: 'bar',
+      encode: {
+        x: 'amount',
+        y: 'product'
+      }
+    }
   ]
 };
