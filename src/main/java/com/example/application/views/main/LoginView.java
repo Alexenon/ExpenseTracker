a8ce40d1e6e944c2ca8cc0431c15c6f6121a7b7c
Alex Xenon <b3haply@gmail.com>
2023-09-01 00:10:54 +0300

    Implemented error message on failed authentication

--- src/main/java/com/example/application/views/main/LoginView.java
+++ src/main/java/com/example/application/views/main/LoginView.java
@@ -3,10 +3,7 @@ package com.example.application.views.main;
 import com.example.application.views.main.components.LoginComponent;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.JsModule;
-import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Main;
-import com.vaadin.flow.component.login.LoginForm;
-import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
@@ -19,29 +16,14 @@ import com.vaadin.flow.server.auth.AnonymousAllowed;
 @JsModule("./themes/light_theme/components/javascript/login-script.js")
 public class LoginView extends Main implements BeforeEnterObserver {
 
-    private final LoginForm loginForm = new LoginForm();
+    private final LoginComponent loginComponent;
 
     public LoginView() {
         addClassName("login-page");
 
-        /*
-        * TODO: Add normal login form
-        * */
+        loginComponent = new LoginComponent();
 
-        FormLayout layout;
-
-        LoginI18n i18n = LoginI18n.createDefault();
-        i18n.getForm().setTitle(null);
-        i18n.setAdditionalInformation(null);
-
-        loginForm.addClassNames("login-form", "remove-style");
-        loginForm.setAction("login");
-        loginForm.setI18n(i18n);
-
-        LoginComponent loginComponent = new LoginComponent();
-
-
-        add(loginComponent, loginForm);
+        add(loginComponent);
 
         UI.getCurrent().getPage().executeJs("addLoginListener()");
     }
@@ -52,7 +34,7 @@ public class LoginView extends Main implements BeforeEnterObserver {
                 .getQueryParameters()
                 .getParameters()
                 .containsKey("error")) {
-            loginForm.setError(true);
+            loginComponent.setError(true);
         }
     }
 }
--- src/main/java/com/example/application/views/main/components/LoginComponent.java
+++ src/main/java/com/example/application/views/main/components/LoginComponent.java
@@ -3,13 +3,17 @@ package com.example.application.views.main.components;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
 
 @Tag("Div")
 public class LoginComponent extends Div {
 
+    private final Div errorWrapper;
+    private final Text errorMessage = new Text("Wrong username or password");
+
     public LoginComponent() {
-        Div errorWrapper = new Div();
+        errorWrapper = new Div();
         errorWrapper.addClassName("error-wrapper");
 
         add(errorWrapper, getComponent());
@@ -19,16 +23,23 @@ public class LoginComponent extends Div {
         return new Html(
                 """
                         <form id="login-form" method="post" action="login">
-                            <span class="error-message" id="error-message"></span>
                             <label for="username">Username</label>
                             <input id="username" type="text" name="username" placeholder="Enter Username" autocomplete="username" required/>
                             <label for="password">Password</label>
                             <input id="password" type="password" name="password" placeholder="Enter Password" autocomplete="current-password" required/>
-                            <button type="submit">Login</button>
+                            <button id="login-btn" type="submit">Login</button>
                         </form>
                         """
         );
 
+    }
+
+    public void setError(boolean error) {
+        if (error) {
+            errorWrapper.add(errorMessage);
+        } else {
+            errorWrapper.remove(errorMessage);
+        }
     }
 
 }
