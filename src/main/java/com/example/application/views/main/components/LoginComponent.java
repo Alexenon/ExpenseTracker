d017f8b72cd1fe961e1a6ee4d8bc969cfea4e407
Alex Xenon <b3haply@gmail.com>
2023-09-03 11:23:13 +0300

    Added login panel

--- src/main/java/com/example/application/views/main/components/LoginComponent.java
+++ src/main/java/com/example/application/views/main/components/LoginComponent.java
@@ -1,10 +1,15 @@
 package com.example.application.views.main.components;
 
+import com.example.application.views.main.RegistrationView;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
 
 @Tag("Div")
 public class LoginComponent extends Div {
@@ -24,43 +29,62 @@ public class LoginComponent extends Div {
 
         Component loginForm = getLoginForm();
 
+        Div panelsContainer = new Div();
+        panelsContainer.addClassName("panels-container");
+        panelsContainer.add(getPanelContent());
+
         signInContainer.add(errorWrapper, loginForm);
-        container.add(signInContainer);
+        container.add(signInContainer, panelsContainer);
 
         add(container);
     }
 
     private Component getLoginForm() {
-        return new Html(
-                """      
-                        <form id="login-form" method="post" action="login" class="sign-in-form">
-                            <h2 class="title">Sign in</h2>
-                            <div class="field email-field">
-                                <div class="input-field">
-                                    <input type="email" placeholder="Enter your email" class="email">
-                                </div>
-                            </div>
-                                
-                            <div class="field create-password">
-                                <div class="input-field">
-                                    <input type="password" placeholder="Enter password" class="password">
-                                    <i class='bx bx-hide show-hide'></i>
-                                </div>
-                            </div>
-                                
-                            <div class="input-field button btn">
-                                <input type="submit" value="Submit Now" />
-                            </div>
-                                
-                            <div class="forgot-password">
-                                <a href="/forgot-password">
-                                    <p>Forgot password?</p>
-                                </a>
-                            </div>
-                        </form>
-                        """
-        );
+        return new Html("""      
+                <form id="login-form" method="post" action="login" class="sign-in-form">
+                    <h2 class="title">Sign in</h2>
+                    <div class="field email-field">
+                        <div class="input-field">
+                            <input type="email" placeholder="Enter your email" class="email">
+                        </div>
+                    </div>
+                    <div class="field create-password">
+                        <div class="input-field">
+                            <input type="password" placeholder="Enter password" class="password">
+                            <i class='bx bx-hide show-hide'></i>
+                        </div>
+                    </div>
+                    <div class="input-field button btn">
+                        <input type="submit" value="Submit Now" />
+                    </div>
+                    <div class="forgot-password">
+                        <a href="/forgot-password">
+                            <p>Forgot password?</p>
+                        </a>
+                    </div>
+                </form>
+                """);
+    }
 
+    private Div getPanelContent() {
+        Div leftPanel = new Div();
+        leftPanel.addClassNames("panel", "left-panel");
+
+        Div content = new Div();
+        content.addClassNames("content");
+
+        H3 h3 = new H3("New here ?");
+        Paragraph p = new Paragraph("Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis, ex ratione. Aliquid!");
+        Button btn = new Button("Sign up", e -> getUI().ifPresent(ui -> ui.navigate(RegistrationView.class)));
+        btn.addClassNames("btn", "transparent");
+
+        Image image = new Image("images/logo_black.png", "Login panel image");
+        image.addClassName("image");
+
+        content.add(h3, p, btn);
+        leftPanel.add(content, image);
+
+        return leftPanel;
     }
 
     public void setError(boolean error) {
