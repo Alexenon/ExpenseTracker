71c78d25ca466cb8071f1b59ac4258f9cd56a394
Alex Xenon <b3haply@gmail.com>
2023-09-04 15:17:35 +0300

    Refactored code for LoginView

--- src/main/java/com/example/application/views/main/LoginView.java
+++ src/main/java/com/example/application/views/main/LoginView.java
@@ -1,7 +1,8 @@
 package com.example.application.views.main;
 
 import com.example.application.views.main.components.LoginComponent;
-import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
@@ -20,8 +21,38 @@ public class LoginView extends Main implements BeforeEnterObserver {
 
         loginComponent = new LoginComponent();
 
-        add(loginComponent);
+        Div container = new Div();
+        container.addClassName("container");
+
+        Div panelsContainer = new Div();
+        panelsContainer.addClassName("panels-container");
+        panelsContainer.add(getPanelContent());
+
+        container.add(loginComponent, panelsContainer);
+        add(container);
     }
+
+    private Div getPanelContent() {
+        Div leftPanel = new Div();
+        leftPanel.addClassNames("panel", "left-panel");
+
+        Div content = new Div();
+        content.addClassNames("content");
+
+        H3 h3 = new H3("New here ?");
+        Paragraph p = new Paragraph("Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis, ex ratione. Aliquid!");
+        Button btn = new Button("Sign up", e -> getUI().ifPresent(ui -> ui.navigate(RegistrationView.class)));
+        btn.addClassNames("btn", "transparent");
+
+        Image image = new Image("images/logo_black.png", "Login panel image");
+        image.addClassName("image");
+
+        content.add(h3, p, btn);
+        leftPanel.add(content, image);
+
+        return leftPanel;
+    }
+
 
     @Override
     public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
--- src/main/java/com/example/application/views/main/components/LoginComponent.java
+++ src/main/java/com/example/application/views/main/components/LoginComponent.java
@@ -1,15 +1,10 @@
 package com.example.application.views.main.components;
 
-import com.example.application.views.main.RegistrationView;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
 
 @Tag("Div")
 public class LoginComponent extends Div {
@@ -18,39 +13,29 @@ public class LoginComponent extends Div {
     private final Text errorMessage = new Text("Wrong username or password");
 
     public LoginComponent() {
+        this.addClassName("signup-container");
+
+        Component loginForm = getLoginForm();
+        loginForm.addClassName("sign-in-form");
+
         errorWrapper = new Div();
         errorWrapper.addClassName("error-wrapper");
 
-        Div container = new Div();
-        container.addClassName("container");
-
-        Div signInContainer = new Div();
-        signInContainer.addClassName("signup-container");
-
-        Component loginForm = getLoginForm();
-
-        Div panelsContainer = new Div();
-        panelsContainer.addClassName("panels-container");
-        panelsContainer.add(getPanelContent());
-
-        signInContainer.add(errorWrapper, loginForm);
-        container.add(signInContainer, panelsContainer);
-
-        add(container);
+        add(errorWrapper, loginForm);
     }
 
     private Component getLoginForm() {
         return new Html("""      
-                <form id="login-form" method="post" action="login" class="sign-in-form">
+                <form id="login-form" method="post" action="login">
                     <h2 class="title">Sign in</h2>
-                    <div class="field email-field">
+                    <div class="field username-field">
                         <div class="input-field">
-                            <input type="email" placeholder="Enter your email" class="email">
+                            <input type="text" name="username" placeholder="Enter your username" class="username">
                         </div>
                     </div>
-                    <div class="field create-password">
+                    <div class="field password-field">
                         <div class="input-field">
-                            <input type="password" placeholder="Enter password" class="password">
+                            <input type="password" name="password" placeholder="Enter password" class="password">
                             <i class='bx bx-hide show-hide'></i>
                         </div>
                     </div>
@@ -64,27 +49,6 @@ public class LoginComponent extends Div {
                     </div>
                 </form>
                 """);
-    }
-
-    private Div getPanelContent() {
-        Div leftPanel = new Div();
-        leftPanel.addClassNames("panel", "left-panel");
-
-        Div content = new Div();
-        content.addClassNames("content");
-
-        H3 h3 = new H3("New here ?");
-        Paragraph p = new Paragraph("Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis, ex ratione. Aliquid!");
-        Button btn = new Button("Sign up", e -> getUI().ifPresent(ui -> ui.navigate(RegistrationView.class)));
-        btn.addClassNames("btn", "transparent");
-
-        Image image = new Image("images/logo_black.png", "Login panel image");
-        image.addClassName("image");
-
-        content.add(h3, p, btn);
-        leftPanel.add(content, image);
-
-        return leftPanel;
     }
 
     public void setError(boolean error) {
