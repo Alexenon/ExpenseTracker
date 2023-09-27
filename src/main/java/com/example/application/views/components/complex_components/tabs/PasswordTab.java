65942912cdfa1ee4d20bde23d6221d62d8eeb67d
Alex Xenon <b3haply@gmail.com>
2023-09-27 14:52:24 +0300

    Started implementing style for settings tab container

--- src/main/java/com/example/application/views/components/complex_components/tabs/PasswordTab.java
+++ src/main/java/com/example/application/views/components/complex_components/tabs/PasswordTab.java
@@ -2,27 +2,40 @@ package com.example.application.views.components.complex_components.tabs;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.textfield.PasswordField;
 import jakarta.annotation.security.PermitAll;
 
 @PermitAll
 public class PasswordTab extends Div {
 
-    private final H1 title = new H1("Change Password");
+    private final H3 title = new H3("Change Password");
     private final PasswordField currentPassword = new PasswordField("Current Password");
     private final PasswordField newPassword = new PasswordField("New Password");
     private final PasswordField confirmPassword = new PasswordField("Confirm New Password");
     private final Button changePasswordButton = new Button("Change Password");
 
     public PasswordTab() {
-        add(getContent());
+        addClassName("tab-container");
+
+        addStyleToElements();
+
+        add(title, getTabContent());
     }
 
-    public Div getContent() {
-        Div div = new Div();
-        div.add(title, currentPassword, newPassword, confirmPassword, changePasswordButton);
+    private void addStyleToElements() {
+        title.addClassName("tab-title");
+
+        currentPassword.addClassName("settings-form-group");
+        newPassword.addClassName("settings-form-group");
+        confirmPassword.addClassName("settings-form-group");
+    }
+
+    private Div getTabContent() {
+        Div div = new Div(currentPassword, newPassword, confirmPassword, changePasswordButton);
+        div.addClassName("settings-password-form");
         return div;
     }
 
 }
+
