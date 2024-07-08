2991b321bae63318e413a096ae425909632bbdd9
Alex Xenon <b3haply@gmail.com>
2024-07-08 22:31:11 +0300

    Implemented MarketCapFetcher

---
+++ src/main/java/com/example/application/utils/MarketCapFetcher.java
@@ -0,0 +1,63 @@
+package com.example.application.utils;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.springframework.http.HttpHeaders;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarketCapFetcher {
+
+    private static final String API_KEY = System.getenv("MARKET_CAP_API_KEY");
+
+    public static void main(String[] args) {
+
+        String uri = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/map";
+        List<NameValuePair> parameters = new ArrayList<>();
+        parameters.add(new BasicNameValuePair("start", "1"));
+        parameters.add(new BasicNameValuePair("limit", "5000"));
+//        parameters.add(new BasicNameValuePair("convert", "USD"));
+
+        try {
+            String result = makeAPICall(uri, parameters);
+            System.out.println(result);
+        } catch (IOException e) {
+            System.out.println("Error: cannot access content - " + e);
+        } catch (URISyntaxException e) {
+            System.out.println("Error: Invalid URL " + e);
+        }
+    }
+
+    public static String makeAPICall(String uri, List<NameValuePair> parameters) throws URISyntaxException, IOException {
+        String response_content;
+
+        URIBuilder query = new URIBuilder(uri);
+        query.addParameters(parameters);
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet request = new HttpGet(query.build());
+
+        request.setHeader(HttpHeaders.ACCEPT, "application/json");
+        request.addHeader("X-CMC_PRO_API_KEY", API_KEY);
+
+        try (CloseableHttpResponse response = client.execute(request)) {
+            System.out.println(response.getStatusLine());
+            HttpEntity entity = response.getEntity();
+            response_content = EntityUtils.toString(entity);
+            EntityUtils.consume(entity);
+        }
+
+        return response_content;
+    }
+
+}
