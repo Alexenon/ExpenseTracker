c0b1d3e3099c866c5422589623d73fbf02c7e11a
Alex Xenon <b3haply@gmail.com>
2024-09-01 18:03:07 +0300

    Added 2 ways to display echarts, without api endpoint calls

--- frontend/themes/light_theme/components/javascript/fillPieChart.js
+++ frontend/themes/light_theme/components/javascript/fillPieChart.js
@@ -109,3 +109,79 @@ window.printData = function printData(url) {
         console.error('Error:', error);
       });
 }
+
+
+window.fillChartPie = function fillChartPie(jsonDataString) {
+  const jsonDataObject = JSON.parse(jsonDataString);
+  const element = document.getElementById('chart-pie');
+  const chart = echarts.init(element);
+
+  var option = {
+    title: {
+      text: 'Expenses Chart',
+      subtext: 'Click on legend to remove a category',
+      left: 'center'
+    },
+    tooltip: {
+      trigger: 'item',
+      className: 'chart-tooltip',
+      formatter(params) {
+        return `
+            <div class="tooltip-content">
+                ${params.marker}
+                <p id="selected-category" class="tooltip-category">${params.name}</p>
+                <p class="tooltip-value">${params.value}</p>
+                <p class="tooltip-currency">USD</p>
+                <div class="tooltip-percent">${params.percent}%</div>
+            </div>
+        `
+      }
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left',
+      // This can be removed
+      borderRadius: 3,
+      borderWidth: 2,
+      padding: 10,
+      itemGap: 10,
+      itemWidth: 25,
+      itemHeight: 14,
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        selectedMode: 'single',
+        radius: '50%',
+        data: jsonDataObject,
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
+
+  // Setting chart style when chart is empty
+  if (jsonObject.length === 0) {
+    option.legend.show = false;
+    option.tooltip.formatter = '0 MDL';
+    option.series[0].color = 'lightgray';
+    option.series[0].data = [{ name: "Empty", value: 0 }];
+  }
+
+  option && chart.setOption(option);
+  window.addEventListener('resize', chart.resize);
+//  showChartDetails();
+
+  // LISTENER FOR CLICKING ON SERIES PIE
+  chart.on('click', 'series.pie', function (params) {
+    console.log(params);
+    // console.log(params.data.name);
+  });
+
+};
--- src/main/java/com/example/application/views/pages/DashboardView.java
+++ src/main/java/com/example/application/views/pages/DashboardView.java
@@ -1,5 +1,6 @@
 package com.example.application.views.pages;
 
+import com.example.application.views.components.complex_components.Chart;
 import com.example.application.views.layouts.MainLayout;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.JavaScript;
@@ -10,7 +11,16 @@ import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import elemental.json.Json;
+import elemental.json.JsonArray;
+import elemental.json.JsonObject;
 import jakarta.annotation.security.PermitAll;
+import org.icepear.echarts.Pie;
+import org.icepear.echarts.charts.pie.PieDataItem;
+import org.icepear.echarts.charts.pie.PieSeries;
+import org.icepear.echarts.components.legend.Legend;
+import org.icepear.echarts.components.title.Title;
+import org.icepear.echarts.components.tooltip.Tooltip;
 
 @PermitAll
 @PageTitle("Dashboard")
@@ -25,18 +35,101 @@ public class DashboardView extends Main {
 
     private void addStyle() {
         addClassName("page-content");
-        HorizontalLayout container = new HorizontalLayout();
-        container.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
 
         Div chartPie = new Div();
         chartPie.setId("chart-pie");
+        testChart();
 
-        /* TODO: Fix fillPieChart URL */
-        UI.getCurrent().getPage().executeJs("fillPie('http://localhost:8080/api/expense/grouped?user=alex')");
-        UI.getCurrent().getPage().executeJs("printData('http://localhost:8080/api/expense/all')");
-
-        container.add(chartPie);
+        HorizontalLayout container = new HorizontalLayout(chartPie);
+        container.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
         add(container);
     }
+
+    private void testChart() {
+        PieDataItem[] data = new PieDataItem[]{
+                new PieDataItem().setName("Category A").setValue(1048),
+                new PieDataItem().setName("Category B").setValue(735),
+                new PieDataItem().setName("Category C").setValue(580),
+                new PieDataItem().setName("Category D").setValue(484),
+                new PieDataItem().setName("Category E").setValue(300)
+        };
+
+        JsonArray jsonOptionData = Json.createArray();
+        for (int i = 0; i < data.length; i++) {
+            JsonObject jsonObject = Json.createObject();
+            jsonObject.put("name", String.valueOf(data[i].getName()));
+            jsonObject.put("value", Double.parseDouble(String.valueOf(data[i].getValue())));
+            jsonOptionData.set(i, jsonObject);
+        }
+
+        UI.getCurrent().getPage().executeJs("console.log($0); fillChartPie($0);", jsonOptionData.toJson());
+
+        System.out.println(jsonOptionData.toJson());
+    }
+
+
+    private void addPieChart() {
+        Title title = new Title()
+                .setText("Expenses Chart")
+                .setSubtext("Click on legend to remove a category")
+                .setLeft("center");
+
+        // Create tooltip
+        Tooltip tooltip = new Tooltip()
+                .setTrigger("item")
+                .setClassName("chart-tooltip")
+                .setFormatter("""
+                        function(params) {
+                             return
+                                 <div class="tooltip-content">
+                                     ${params.marker}
+                                     <p id="selected-category" class="tooltip-category">${params.name}</p>
+                                     <p class="tooltip-value">${params.value}</p>
+                                     <p class="tooltip-currency">MDL</p>
+                                     <div class="tooltip-percent">${params.percent}%</div>
+                                 </div>
+                         }
+                         """);
+
+
+        // Create legend
+        Legend legend = new Legend()
+                .setOrient("vertical")
+                .setLeft("left")
+                .setPadding(10)
+                .setItemGap(10)
+                .setItemWidth(25)
+                .setItemHeight(14);
+
+
+        PieDataItem[] data = new PieDataItem[]{
+                new PieDataItem().setName("Category A").setValue(1048),
+                new PieDataItem().setName("Category B").setValue(735),
+                new PieDataItem().setName("Category C").setValue(580),
+                new PieDataItem().setName("Category D").setValue(484),
+                new PieDataItem().setName("Category E").setValue(300)
+        };
+
+
+        // Create series
+        PieSeries series = new PieSeries()
+                .setName("Access From")
+                .setType("pie")
+                .setSelectedMode("single")
+                .setRadius("50%")
+                .setData(data);
+
+
+        // Create pie chart
+        Pie pie = new Pie()
+                .setTitle(title)
+                .setTooltip(tooltip)
+                .setLegend(legend)
+                .addSeries(series);
+
+        Chart c = new Chart("pie-cool", pie.getOption());
+        add(c);
+    }
+
 
 }
