5a93018ba67d61443045ec8deb76a95a501f0906
Alexenon <Alexeneon@gmail.com>
2026-05-04 01:28:55 +0300

    Starting implementing ContactUs page

--- src/main/java/com/example/application/views/components/custom/Footer.java
+++ src/main/java/com/example/application/views/components/custom/Footer.java
@@ -3,6 +3,7 @@ package com.example.application.views.components.custom;
 import com.example.application.views.components.core.Container;
 import com.example.application.views.components.custom.icons.MonoIcon;
 import com.example.application.views.components.custom.icons.PictogramIcon;
+import com.example.application.views.pages.ContactView;
 import com.example.application.views.pages.PolicyView;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.html.Span;
@@ -10,38 +11,39 @@ import com.vaadin.flow.router.RouterLink;
 
 public class Footer extends com.vaadin.flow.component.html.Footer {
 
-    private final RouterLink termsLink = new RouterLink("Terms of Service", PolicyView.class);
-    private final RouterLink policyLink = new RouterLink("Privacy Policy", PolicyView.class);
-    private final MonoIcon githubIcon = PictogramIcon.GITHUB.create();
-    private final MonoIcon facebookIcon = PictogramIcon.FACEBOOK.create();
-    private final MonoIcon twitterIcon = PictogramIcon.TWITTER.create();
+	private final RouterLink termsLink = new RouterLink("Terms of Service", PolicyView.class);
+	private final RouterLink policyLink = new RouterLink("Privacy Policy", PolicyView.class);
+	private final RouterLink contactLink = new RouterLink("Contact us", ContactView.class);
+	private final MonoIcon githubIcon = PictogramIcon.GITHUB.create();
+	private final MonoIcon facebookIcon = PictogramIcon.FACEBOOK.create();
+	private final MonoIcon twitterIcon = PictogramIcon.TWITTER.create();
 
-    public Footer() {
-        addClassName("footer");
-        add(
-                copywriteArea(),
-                plainLinkArea(),
-                iconsLinkArea()
-        );
-    }
+	public Footer() {
+		addClassName("footer");
+		add(
+				copywriteArea(),
+				plainLinkArea(),
+				iconsLinkArea()
+		);
+	}
 
-    private Container copywriteArea() {
-        return Container.builder("footer-copywrite", "centered-row")
-                .addComponent(new Span("©"))
-                .addComponent(new Paragraph("All rights reserved."))
-                .build();
-    }
+	private Container copywriteArea() {
+		return Container.builder("footer-copywrite", "centered-row")
+				.addComponent(new Span("©"))
+				.addComponent(new Paragraph("All rights reserved."))
+				.build();
+	}
 
-    private Container plainLinkArea() {
-        return Container.builder("footer-links", "centered-row")
-                .addComponents(termsLink, policyLink)
-                .build();
-    }
+	private Container plainLinkArea() {
+		return Container.builder("footer-links", "centered-row")
+				.addComponents(contactLink, termsLink, policyLink)
+				.build();
+	}
 
-    private Container iconsLinkArea() {
-        return Container.builder("social-icons")
-                .addComponents(githubIcon, facebookIcon, twitterIcon)
-                .build();
-    }
+	private Container iconsLinkArea() {
+		return Container.builder("social-icons")
+				.addComponents(githubIcon, facebookIcon, twitterIcon)
+				.build();
+	}
 
 }
